3863e07f5ebf9f75fbcd5672180c38ef65d54fae
wslyvh <wslyvh@users.noreply.github.com>
2023-03-17 17:26:17 +0000

    update types

--- src/attestation-station.ts
+++ src/attestation-station.ts
@@ -1,20 +1,22 @@
 import { AttestationCreated as AttestationCreatedEvent } from "../generated/AttestationStation/AttestationStation"
 import { Attestation } from "../generated/schema"
-import { parseBytes32String, toUtf8String } from '@ethersproject/strings'
 
 export function handleAttestationCreated(event: AttestationCreatedEvent): void {
   let entity = new Attestation(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+  
   entity.creator = event.params.creator
   entity.receiver = event.params.about
   entity.key = event.params.key
-  entity.keyString = parseBytes32String(event.params.key)
-  entity.value = toUtf8String(event.params.val)
+  entity.keyString = event.params.key.toString()
+  entity.val = event.params.val
+  entity.value = event.params.val.toString()
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+    
 }
